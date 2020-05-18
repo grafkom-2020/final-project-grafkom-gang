de35d352d035bc71e0d1a88083ad66110005e403
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-05-18 14:26:50 +0700

    added some object

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ initScene = function() {
     addKeyToListen("KeyD");
 
     renderer.domElement.requestPointerLock();
+
+    var capsule = new Physijs.CapsuleMesh(
+        new THREE.CylinderGeometry(0.3,0.3,2,32), 
+        new THREE.MeshBasicMaterial({ color: 0x888888 }),
+    );
+    capsule.name = "Player Mesh";
+    // capsule.position.y += 2;
+    // capsule.__dirtyPosition = true;
+    scene.add(capsule);
+
     player = new Player(camera);
     scene.add(player.getObject());
 
@@ -38,11 +48,24 @@ initScene = function() {
         player.deactivate();
     },false);
     
+    var box = new Physijs.BoxMesh(
+        new THREE.CubeGeometry(4,4,4),
+        new THREE.MeshBasicMaterial({ color: 0x888888 }),
+        0
+    );
+    // box.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
+    //     console.log(other_object);
+    //     console.log(relative_velocity);
+    //     console.log(relative_rotation);
+    //     console.log(contact_normal);
+    // });
+    scene.add(box);
+    // box.position.set(0,-1.5,0);
+    // box.__dirtyPosition = true;
     requestAnimationFrame( render );
 };
 
 render = function() {
-
     scene.simulate(); // run physics
     update();
     renderer.render(scene, camera); // render the scene
