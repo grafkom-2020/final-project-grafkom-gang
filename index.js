a4263122a7aebcf67afb076a73e80b35a9b09b20
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-05-18 21:30:39 +0700

    state aneh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 	
 Physijs.scripts.worker = '/lib/js/physijs_worker.js';
-Physijs.scripts.ammo = '/lib/js/ammo.js';
+Physijs.scripts.ammo = '/lib/js/ammo.min.js';
 
 var initScene, render, objloader, texloader, renderer, player;
 
@@ -29,15 +29,16 @@ initScene = function() {
 
     renderer.domElement.requestPointerLock();
 
-    var capsule = new Physijs.CylinderMesh(
-        new THREE.CylinderGeometry(0.3,0.3,2,32), 
+    var capsule = new Physijs.BoxMesh(
+        new THREE.CubeGeometry(0.3,2,0.3),
         new THREE.MeshBasicMaterial({ color: 0x888888 }),
     );
+    // capsule.__dirtyPosition = true;
     capsule.name = "Player Mesh";
-    capsule.position.y += 4;
+    capsule.position.y += 2;
     scene.add(capsule);
 
-    player = new Player(camera);
+    player = new Player(camera,capsule);
     scene.add(player.getObject());
 
     document.addEventListener('click', function(){
@@ -47,13 +48,18 @@ initScene = function() {
         player.deactivate();
     },false);
     
-    var box = new Physijs.BoxMesh(
-        new THREE.CubeGeometry(4,4,4),
+    var floor = new Physijs.BoxMesh(
+        new THREE.CubeGeometry(9,0.1,11),
         new THREE.MeshBasicMaterial({ color: 0x888888 }),
         0
     );
-    box.position.set(0,-1.5,0);
-    scene.add(box);
+    floor.position.set(0,-0.04,0);
+    floor.addEventListener('collision', function(other_object, relative_vel, relative_rot, contact_normal){
+        other_object.setLinearVelocity(new THREE.Vector3(0,0,0));
+        other_object.setAngularVelocity(new THREE.Vector3(0,0,0));
+    });
+    // box.__dirtyPosition = true;
+    scene.add(floor);
     requestAnimationFrame( render );
 };
 
--- player.js
+++ player.js
@@ -1,10 +1,22 @@
 
-function Player (_camera){
+function Player (_camera,_mesh){
 
     let control = new THREE.PointerLockControls(_camera, document.body);
     let speed = 0.05;
     let isActive = false;
+    let dir = new THREE.Vector3();
 
+    _mesh.setLinearFactor(new THREE.Vector3(0,0,0));
+    _mesh.setAngularFactor(new THREE.Vector3(0,0,0));
+    _mesh.add(_camera);
+    _camera.position.set(0,1,0);
+    _mesh.addEventListener('collision', function(other_object, relative_velocity, relative_rotation, contact_normal){
+        xColl = relative_velocity.x * contact_normal.x;
+        zColl = relative_velocity.z * contact_normal.z;
+        console.log(xColl);
+        console.log(zColl);
+        coll = true;
+    });
     this.activate = function(){
         if(!isActive){
             control.lock();
@@ -17,11 +29,14 @@ function Player (_camera){
             isActive = false;
         }
     }
+    this.mesh = function(){
+        return _mesh;
+    }
     this.update = function(){
-        let deltaFordward = 0;
+        let deltaForward = 0;
         let deltaRight = 0;
         if(isKey("KeyW")){
-            deltaFordward += speed;
+            deltaForward += speed;
         }
         
         if(isKey("KeyA")){
@@ -29,18 +44,35 @@ function Player (_camera){
         }
     
         if(isKey("KeyS")){
-            deltaFordward -= speed;
+            deltaForward -= speed;
         }
     
         if(isKey("KeyD")){
             deltaRight += speed;
         }
 
-        control.moveForward(deltaFordward);
+        control.getDirection(dir);
+        dir.normalize();
+        dir.x += deltaForward;
+        dir.z += deltaRight
+        _mesh.position.set(dir);
+        control.moveForward(deltaForward);
         control.moveRight(deltaRight);
+
+        // dir.setFromMatrixColumn( _camera.matrix, 0 );
+
+		// dir.crossVectors( _camera.up, dir );
+
+        // _mesh.position.addScaledVector( dir, deltaForward );
+        
+        // dir.setFromMatrixColumn( _camera.matrix, 0 );
+
+        // _mesh.position.addScaledVector( dir, deltaRight );
+        
+        // _mesh.__dirtyPosition = true;
     }
 
     this.getObject = function(){
-        return control.getObject();
+        return _mesh;
     }
 }
